567bc971b59c804e12804a143ad222b21436b15a
Rafael Magana <raf.magana@gmail.com>
2010-09-12 23:01:39 -0500

    done sinatra app for the facebook tutorial

--- faceboku.rb
+++ faceboku.rb
@@ -3,15 +3,15 @@ require 'omniauth/oauth'
 
 #use OmniAuth::Strategies::Facebook, 'ccfda574d92e83a704ac982b33617f01', '7a7663099ccb62f180d985ba1252a3e2'
 
+#Here you have to put your own Application ID and Secret
+APP_ID = '153304591365687'
+APP_SECRET = '7a7663099ccb62f180d985ba1252a3e2'
+
 get '/' do
-  #if params["fb_sig_in_iframe"]
     @articles = []
     @articles << {:title => 'Deploying Rack-based apps in Heroku', :link => 'http://docs.heroku.com/rack/'}
     @articles << {:title => 'Learn Ruby in twenty minutes', :link => 'http://www.ruby-lang.org/en/documentation/quickstart/'}
     erb :index
-  #else
-  #  redirect 'http://apps.facebook.com/facebooku'
-  #end
 end
 
 get '/auth/facebook/callback' do
