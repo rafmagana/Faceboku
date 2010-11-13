e15cf0ef4e66c55fa4c3ea641d531ff795d71e6f
Rafael Magana <raf.magana@gmail.com>
2010-11-12 22:29:22 -0600

    add open graph and OAuth

--- faceboku.rb
+++ faceboku.rb
@@ -1,20 +1,44 @@
 require 'sinatra'
 require 'omniauth/oauth'
 
-#use OmniAuth::Strategies::Facebook, 'ccfda574d92e83a704ac982b33617f01', '7a7663099ccb62f180d985ba1252a3e2'
+enable :sessions
 
-#Here you have to put your own Application ID and Secret
-APP_ID = '153304591365687'
-APP_SECRET = '7a7663099ccb62f180d985ba1252a3e2'
+APP_ID = "153304591365687"
+APP_SECRET = "7a7663099ccb62f180d985ba1252a3e2"
+
+use OmniAuth::Builder do  
+  provider :facebook, APP_ID, APP_SECRET, { :scope => 'email, status_update, publish_stream' }
+end
 
 get '/' do
     @articles = []
-    @articles << {:title => 'Deploying Rack-based apps in Heroku', :link => 'http://docs.heroku.com/rack/'}
-    @articles << {:title => 'Learn Ruby in twenty minutes', :link => 'http://www.ruby-lang.org/en/documentation/quickstart/'}
+    @articles << {:title => 'Introduction to Heroku', :url => 'http://docs.heroku.com/heroku'}
+    @articles << {:title => 'Deploying Rack-based apps in Heroku', :url => 'http://docs.heroku.com/rack'}
+    @articles << {:title => 'Learn Ruby in twenty minutes', :url => 'http://www.ruby-lang.org/en/documentation/quickstart/'}
+    
     erb :index
 end
 
 get '/auth/facebook/callback' do
-  #request.cookies["fb_"]
-  #params[:auth].inspect
+  session['fb_auth'] = request.env['omniauth.auth']
+  session['fb_token'] = session['fb_auth']['credentials']['token']
+  session['fb_error'] = nil
+  redirect '/'
+end
+
+get '/auth/failure' do
+  clear_session
+  session['fb_error'] = 'In order to use this site you must allow us access to your Facebook data<br />'
+  redirect '/'
+end
+
+get '/logout' do
+  clear_session
+  redirect '/'
+end
+
+def clear_session
+  session['fb_auth'] = nil
+  session['fb_token'] = nil
+  session['fb_error'] = nil
 end
